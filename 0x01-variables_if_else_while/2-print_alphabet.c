327f35a9e1b16a4fc51d2cadf1a54fd601e07402
Leleji Godwin <degoldmyne@gmail.com>
2022-10-15 01:18:16 -0700

    Added aplhabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (char start='a'; start<='z'; start++)
+		putchar(start);
+		putchar('\n');
+
+	return (0)
+}
